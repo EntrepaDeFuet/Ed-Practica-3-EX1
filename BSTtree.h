53b1d4b4711e13a8da507127f6a8208a67afdba2
EntrepaDeFuet <Orioljb19992gmail.com>
2025-05-15 12:30:56 +0200

    ED Practica3EX1

---
+++ BSTtree.h
@@ -0,0 +1,221 @@
+#ifndef BSTTREE_H
+#define BSTTREE_H
+#include "NODEtree.h"
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+
+
+
+template <class Key, class Value>
+
+class BSTtree {
+
+public:
+
+BSTtree();
+BSTtree(const BSTtree<Key, Value>& orig);
+virtual ~BSTtree();
+bool empty() const;
+int size() const;
+int height() const;
+NODEtree<Key,Value>* insert(const Key& k, const Value& value);
+const vector<Value>& valuesOf(const Key& k) const;
+void printPreorder(const NODEtree<Key,Value>* n = nullptr) const;
+void printInorder(const NODEtree<Key,Value>* n = nullptr) const;
+void printPostorder(const NODEtree<Key,Value>* n = nullptr) const;
+void printSecondLargestKey() const;
+void mirrorTree();
+list<NODEtree<Key, Value>*> getLeafNodes() const;
+
+protected:
+
+NODEtree<Key,Value>* root;
+NODEtree<Key,Value>* search(const Key& k) const;
+
+private:
+
+int _size;
+NODEtree<Key,Value> copia(const NODEtree<Key,Value>* orig);
+NODEtree<Key,Value> insertaNode(NODEtree<Key,Value>* orig,NODEtree<Key,Value>* node);
+const vector<Value>& valuesOfRecursiu(const NODEtree<Key,Value> orig,const Key& k) const;
+void deleteNode(NODEtree<Key,Value>* node) const;
+void buscarElGros(const NODEtree<Key,Value>* node)const
+};
+
+template <class Key, class Value>
+BSTtree<Key,Value>::BSTtree(){
+    this->root = nullptr;
+    this->_size= 0;
+
+}
+template <class Key, class Value>
+NODEtree<Key,Value> BSTtree<Key,Value>::copia(const NODEtree<Key,Value>* orig){
+
+    NODEtree<Key,Value>* node = new NODEtree<Key,Value>(orig);
+    if(orig.hasLeft()){
+        node.setLeft(copia(orig->getLeft()));
+        node.getLeft()->setFather(node);
+    }
+    if(orig.hasRight()){
+        node.setRight(copia(orig->getRight()));
+        node.getRight()->setFather(node);
+
+    }
+    return node;
+}
+
+template <class Key, class Value>
+BSTtree<Key,Value>::BSTtree(const BSTtree<Key,Value>& orig){
+    this->root = copia(orig->root());
+    this->size = orig.size();
+}
+
+template<class Key,class Value>
+void BSTtree<Key,Value>::deleteNode(NODEtree<Key,Value>* node)const{
+    if(node->getLeft()!=nullptr){
+        deleteNode(node->getLeft());
+    }
+    if(node->getRight()!=nullptr){
+        deleteNode(node->getRight());
+    }
+    delete node;
+}
+
+
+template <class Key, class Value>
+BSTtree<Key,Value>::~BSTtree(){
+    
+}
+template <class Key, class Value>
+
+bool BSTtree<Key,Value>::empty() const{
+    return this->root == nullptr;
+}
+template <class Key, class Value>
+int BSTtree<Key,Value>::size() const{
+    return this->size;
+}
+template <class Key, class Value>
+int BSTtree<Key,Value>::height()const{
+    return this->root->height();
+}
+template <class Key, class Value>
+NODEtree<Key,Value>* BSTtree<Key,Value>::insert(const Key& k, const Value& value){
+    NODEtree<Key,Value>* node = new NODEtree<Key,Value>();
+    node->setKey(k);
+    node->insertValue(value);
+    return insertaNode(this->root,node);
+}
+template <class Key, class Value>
+NODEtree<Key,Value> insertaNode(NODEtree<Key,Value>* orig,NODEtree<Key,Value>* node){
+
+    if(orig == node){
+        orig->insertValue(node->getValue()[0]);
+        return orig;
+    } else if(orig->getKey()<node->getKey()){
+        if(orig->getRight()==nullptr){
+            orig->setRight(node);
+            orig->getRight()->setFather(orig);
+            return orig->getRight();
+        } else {
+            return insertaNode(orig->getRight(),node);
+        }
+
+    } else if(orig->getKey()>node->getKey()){
+        if(orig->getLeft()==nullptr){
+            orig->setLeft(node);
+            orig->getLeft()->setFather(orig);
+            return orig->getLeft();
+        } else {
+            return insertaNode(orig->getLeft(),node);
+        }
+    }
+}
+
+template<class Key, class Value>
+const vector<Value>& BSTtree<Key,Value>::valuesOf(const Key& k) const{
+    return valuesOfRecursiu(this->root,k);
+}
+template<class Key, class Value>
+const vector<Value>& BSTtree<Key,Value>::valuesOfRecursiu(const NODEtree<Key,Value> orig,const Key& k) const{
+    if(orig == nullptr){
+
+        throw string("No hi ha cap valor amb aquesta clau");
+
+    }else if(orig.getKey()==k){
+
+        return orig.getValue();
+
+    } else {
+        if(k>orig.getKey()){
+            return valuesOfRecursiu(orig.getRight(),k);
+        } else {
+            return valuesOfRecursiu(orig.getLeft(),k);
+        }
+    }
+}
+template<class Key, class Value>
+void BSTtree<Key,Value>::printPreorder(const NODEtree<Key,Value>* n) const{
+    if(n == nullptr && this.root == nullptr){
+        return;
+    } else if(n==nullptr){
+        printPreorder(this->root);
+    } else {
+        cout << n->getValue().toString();
+        if(n->getLeft()!=nullptr){
+            printPreorder(n->getLeft());
+        }
+        if(n->getRight()!=nullptr){
+            printPreorder(n->getRight());
+        }
+    }
+}
+template<class Key, class Value>
+void BSTtree<Key,Value>::printInorder(const NODEtree<Key,Value>* n) const{
+    if(n == nullptr && this.root == nullptr){
+        return;
+    } else if(n==nullptr){
+        printInorder(this->root);
+    } else {
+        if(n->getLeft()!=nullptr){
+            printInorder(n->getLeft());
+        }
+        cout << n->getValue().toString();
+        if(n->getRight()!=nullptr){
+            printInorder(n->getRight());
+        }
+    }
+}
+template<class Key, class Value>
+void BSTtree<Key,Value>::printPostorder(const NODEtree<Key,Value>* n) const{
+    if(n == nullptr && this.root == nullptr){
+        return;
+    } else if(n==nullptr){
+        printPostorder(this->root);
+    } else {
+        if(n->getLeft()!=nullptr){
+            printPostorder(n->getLeft());
+        }
+        
+        if(n->getRight()!=nullptr){
+            printPostorder(n->getRight());
+        }
+        cout << n->getValue().toString();
+    }
+}
+template<class Key,class Value>
+void BSTtree<Key,Value>::buscarElGros(const NODEtree<Key,Value>* node)const{
+    if(node->getRight()==nullptr){
+        cout << node->getFather()->getValue().toString() << endl;
+    } else {
+        buscarElGros(node->getRight())
+    }
+}
+template<class Key,class Value>
+void BSTtree<Key,Value>::printSecondLargestKey() const{
+    buscarElGros(this->root);
+}
+#endif
---
+++ NODEtree.h
@@ -0,0 +1,190 @@
+#ifndef NODETREE_H
+#define NODETREE_H
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+template <class Key, class Value>
+
+
+class NODEtree {
+
+public:
+
+NODEtree(const Key& key);
+NODEtree(const NODEtree<Key,Value>& orig);
+virtual ~NODEtree();
+
+/* Modificadors */
+void setRight(const NODEtree<Key,Value>* node);
+void setLeft(const NODEtree<Key,Value>* node);
+void setKey( Key _key);
+
+
+/* Consultors */
+const Key& getKey() const;
+const vector<Value>& getValue() const;
+const NODEtree <Key,Value>* getLeft() const;
+const NODEtree <Key, Value>* getRight() const;
+
+
+
+bool isRoot() const;
+bool hasLeft() const;
+bool hasRight() const;
+bool isExternal() const;
+void insertValue(const Value& v);
+int depth() const;
+int height() const;
+bool operator==(const NODEtree<Key,Value>& node) const;
+
+// Mètodes pròpis per a encapsular codi:
+const NODEtree <Key,Value>* getFather() const;
+bool mateixaLlista(vector<Value> llista) const;
+void setFather(const NODEtree<Key,Value>* node);
+
+private:
+
+Key key;
+vector<Value> values;
+NODEtree<Key, Value>* left;
+NODEtree<Key,Value>* right;
+//Sense l'atribut pare no he sapigut com veure si es root o calcular la depth.
+NODEtree<Key, Value>* father;
+
+};
+//Constructors:
+
+template <class Key, class Value>
+NODEtree<Key,Value>::NODEtree(const Key& key){
+    this->key = key;
+}
+
+template <class Key, class Value>
+NODEtree<Key,Value>::NODEtree(const NODEtree<Key,Value>& orig){
+    this->key = orig.getKey();
+    this->values = orig.getValue();
+    this->father = orig.getFather();
+}
+
+template <class Key, class Value>
+void NODEtree<Key,Value>::setRight(const NODEtree<Key,Value>* node){
+    this->right = node;
+}
+
+//Destructor:
+template <class Key, class Value>
+NODEtree<Key,Value>::~NODEtree(){}
+
+//Modificadors:
+
+template <class Key, class Value>
+void NODEtree<Key,Value>::setLeft(const NODEtree<Key,Value>* node){
+    this->left = node;
+}
+template <class Key, class Value>
+void NODEtree<Key,Value>::setKey(Key _key){
+    this->key = _key;
+}
+//Consultors:
+template <class Key, class Value>
+const Key& NODEtree<Key,Value>::getKey() const{
+    return this->Key;
+}
+template <class Key, class Value>
+const vector<Value>& NODEtree<Key,Value>::getValue() const{
+    return this->values;
+}
+
+template <class Key, class Value>
+const NODEtree<Key,Value>* NODEtree<Key,Value>::getRight() const{
+    return this->right;
+}
+template <class Key, class Value>
+const NODEtree<Key,Value>* NODEtree<Key, Value>::getLeft() const{
+    return this->left;
+}
+template <class Key,class Value>
+const NODEtree <Key,Value>*  NODEtree<Key, Value>::getFather() const{
+    return this->father;
+}
+
+//Altres mètodes.
+
+template <class Key, class Value>
+bool NODEtree<Key,Value>::isRoot()const{
+    if(this->getFather()== nullptr){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+template <class Key, class Value>
+bool NODEtree <Key, Value>::hasLeft() const{
+    return this->getLeft()!=nullptr;
+}
+
+template <class Key, class Value>
+bool NODEtree <Key, Value>::hasRight() const{
+    return this->getRight()!=nullptr;
+}
+
+template <class Key, class Value>
+bool NODEtree <Key, Value>::isExternal() const{
+    return !this->hasLeft() && !this->hasRight();
+}
+
+template <class Key, class Value>
+void NODEtree <Key, Value>::insertValue(const Value& value){
+    Value value1 = value;
+    this->getValue().push_back(value1);
+}
+
+template <class Key, class Value>
+int NODEtree <Key, Value>::depth()const{
+    if(this->isRoot()){
+        return 0;
+    } else {
+        return 1 + this->father->depth();
+    }
+}
+
+template <class Key, class Value>
+int NODEtree <Key, Value>::height() const{
+    if(this->isExternal()){
+        return 1;
+    } else {
+        return 1 + max(this->getLeft()->height(),this->getRight()->height());
+    }
+}
+template <class Key, class Value>
+bool NODEtree <Key, Value>::operator==(const NODEtree <Key,Value>& node) const{
+
+    return this->getKey()==node.getKey() && this->mateixaLlista(node.getValue());
+
+}
+
+template <class Key, class Value>
+bool NODEtree<Key,Value>::mateixaLlista(vector<Value> llista) const{
+    bool b = true;
+    int n;
+    int i = 0;
+    if(llista.size()==this->values.size()){
+        n = llista.size;
+    } else {
+        b = false;
+    }
+    while(b && i < n){
+        b = llista[i] == this->values[i];
+        i++;
+    }
+    return b;
+}
+template <class Key, class Value>
+void NODEtree<Key,Value>::setFather(const NODEtree<Key,Value>* node){
+    this->father = node;
+}
+
+#endif
